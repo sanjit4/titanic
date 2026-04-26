b6d8540d4bb5d929c1b979c3802de0266b33616e
Sanjit <sanjitgurung94@gmail.com>
2026-04-26 23:59:43 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+path = "dataset/titanic"
+
+# The file should have exactly 2 columns:
+
+# PassengerId (sorted in any order)
+# Survived (contains your binary predictions: 1 for survived, 0 for deceased)
+
+
